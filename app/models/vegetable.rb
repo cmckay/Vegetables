4c28caaffe8933bf2557daa5736481a076304dcd
Chris McKay <christopher.a.mckay@gmail.com>
2013-02-16 15:34:20 -0600

    added style css file

--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -22,4 +22,25 @@ class Vegetable < ActiveRecord::Base
 
     return companion_vegetables
   end
+
+    def antagonisms
+    left_side_relationships = Antagonisms.where(:first_id => self.id)
+    right_side_relationships = Antagonisms.where(:second_id => self.id)
+    return left_side_relationships + right_side_relationships
+  end
+
+  def companions
+    antagonist_vegetables = []
+
+    left_side_relationships = Antagonisms.where(:first_id => self.id)
+    left_side_relationships.each do |antagonisms|
+      antagonsit_vegetables << antagonisms.second
+    end
+
+    right_side_relationships = Antagonisms.where(:second_id => self.id)
+    right_side_relationships.each do |antagonisms|
+      antagonist_vegetables << antagonisms.first
+    end
+
+    return antagonist_vegetables
 end
