c92552a9a2a2f897a5101080144f6c36ac808049
Student <student@TSL-iMac-06.local>
2013-02-16 17:15:41 -0600

    Fixed indentations

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(event) {
+//  $('select').parent.addClass('compatible');
+// });
+
+// $('select option:selected') {
+//     $(this).parent.addClass('compatible');
+// });
