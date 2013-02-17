576ef19c6e7a8ec7ce968ecffa24914d779ef3a0
Student <student@TSL-iMac-06.local>
2013-02-16 18:16:57 -0600

    style changes. parenthesis!

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,7 +1,14 @@
-// $(document).ready(function(event) {
-//  $('select').parent.addClass('compatible');
-// });
+$(document).ready(function(event) {
+  $('select').change(function(event){
+    _self = $(this);
+    if (_self.val() == 'vegetable')
+      _self.parent().addClass('compatible');
+    else
+      _self.parent().removeClass('compatible');
+  });
+});
 
-// $('select option:selected') {
-//     $(this).parent.addClass('compatible');
-// });
+
+$('header')
+$('.bubble')
+$('#container')
