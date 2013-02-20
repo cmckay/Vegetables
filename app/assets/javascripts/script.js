8f2ba35ddb074a53222b7b8e4124ed9bc1ab491e
Karen Wei <karen.t.wei@gmail.com>
2013-02-20 01:13:35 -0600

    Playing around with tabs using jquery

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,6 +6,19 @@ $(document).ready(function(event) {
     else
       _self.parent().removeClass('compatible');
   });
+
+  $('.plot-container div').hide();
+  $('.plot-container div:first').show();
+  $('.tab-nav li:first').addClass('tab-active');
+
+  $('.tab-nav a').on('click', function(event) {
+	  event.preventDefault();
+	  $('.tab-nav li').removeClass('tab-active');
+	  $(this).parent().addClass('tab-active');
+	  $('.plot-container div').hide();
+	  $($(this).attr('href')).show();
+	});
+
 });
 
 
